1509f37e70cd4a27a5826cfc104c8098b269c2ad
toy-ruby <toy.ruby@gmail.com>
2016-12-23 22:58:00 -0600

    colors, man, colors

--- Test01/Scripts/stockquote.js
+++ Test01/Scripts/stockquote.js
@@ -1,4 +1,25 @@
-﻿$(document).ready(function()
+﻿$(document).ready(function ()
 {
+    $('#btnGet').click(function (event)
+    {
+        var req = new Object();
+        req.input = $('#symbolText').val();
 
+        var url = "http://dev.markitondemand.com/Api/v2/Lookup";
+        $.ajax(
+        {
+            type: "GET",
+            url: url,
+            contentType: "application/x-www-form-urlencoded",
+            data: req.symbol,
+            success: function ()
+            {
+                $("#outputText").val("");
+            },
+            error: function ()
+            {
+                alert(xmlRequest.status + " " + xmlRequest.statusText + " " + xmlRequest.responseText);
+            }
+        })
+    });
 });
--- Test01/stockquote.aspx
+++ Test01/stockquote.aspx
@@ -1,8 +1,10 @@
 ﻿<%@ Page Title="Stock Quote" Language="C#" MasterPageFile="~/Test01.Master" AutoEventWireup="true" CodeBehind="stockquote.aspx.cs" Inherits="Test01.stockquote" %>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <label>Stock Quote:</label>
-    <input id="btnGet" type="text" placeholder="Stock Symbol" />
-    <input type="button" value="Get" />
+    <input id="symbolText" type="text" placeholder="Stock Symbol" />
+    <input id="btnGet" type="button" value="Get" />
+    <p id="outputText"></p>
 </asp:Content>
